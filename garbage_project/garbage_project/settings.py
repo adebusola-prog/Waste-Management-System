e5ec2bc1b99bc693c8920a2c68886b1d074faea2
zeu <aadeyeye@afexnigeria.com>
2023-05-12 20:07:24 +0100

    another commit

--- garbage_project/garbage_project/settings.py
+++ garbage_project/garbage_project/settings.py
@@ -89,7 +89,7 @@ CRISPY_TEMPLATE_PACK = 'uni_form'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
