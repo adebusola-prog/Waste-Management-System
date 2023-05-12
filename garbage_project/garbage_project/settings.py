be0ea842c0d4cfce1e226dde760e2b777939f981
zeu <aadeyeye@afexnigeria.com>
2023-05-12 16:55:09 +0100

    commit it

--- garbage_project/garbage_project/settings.py
+++ garbage_project/garbage_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", cast=bool)
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['https://garbage-management-system-production.up.railway.app/', '127.0.0.1']
+ALLOWED_HOSTS = ['https://garbage-management-system-production.up.railway.app/', '*']
 
 
 # Application definition
